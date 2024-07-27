4264e11f8094eee879a6a530271bb1758f9a4622
cecibunkin <cecibunkin@gmail.com>
2024-07-27 13:18:43 +0200

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,9 @@
+import useGenre from "../hooks/useGenre";
+
+const GenreList = () => {
+  const { genres } = useGenre();
+
+  return genres.map((genre) => <li key={genre.id}>{genre.name}</li>);
+};
+
+export default GenreList;
