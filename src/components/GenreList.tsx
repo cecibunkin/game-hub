aa027a14642d2461236035b50d702df2ad5ccab6
cecibunkin <cecibunkin@gmail.com>
2024-08-03 11:16:13 +0200

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,26 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
+import GetCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenre();
 
-  return data.map((genre) => <li key={genre.id}> {genre.name}</li>);
+  return (
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={GetCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
 };
 
 export default GenreList;
