f3c02c7c439c6d44ab2f7ddf4e1076cf3782ad33
cecibunkin <cecibunkin@gmail.com>
2024-08-11 14:32:20 +0200

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,9 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>{colorMode === "dark" ? "Light" : "Dark"} mode</Text>
+      <Text whiteSpace="nowrap">
+        {colorMode === "dark" ? "Light" : "Dark"} mode
+      </Text>
     </HStack>
   );
 };
